566f1bf4a9c94d3d08f7738124d105609639fbdd
AleksandarZhekovski <aleksandarzhekovski@protonmail.com>
2025-09-15 20:57:49 +0300

    fixed tuigreet pathing

--- hosts/Rivendell/config.nix
+++ hosts/Rivendell/config.nix
@@ -11,7 +11,7 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
-    ../tuigreet/greeter.nix 
+    ../../whatnamethis/tuigreet/greeter.nix 
   ];
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
