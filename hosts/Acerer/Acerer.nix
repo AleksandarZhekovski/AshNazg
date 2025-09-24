d233c53e96eb39f1dd0d6f45aace13a5bca55735
AleksandarZhekovski <aleksandarzhekovski@protonmail.com>
2025-09-24 22:03:35 +0300

    Acerer: Nvidia driver fr this time and added dimitar user

--- hosts/Acerer/Acerer.nix
+++ hosts/Acerer/Acerer.nix
@@ -29,11 +29,21 @@
 
   time.timeZone = "Europe/Sofia";
 
-  users.users.alex = {
-    isNormalUser = true;
-    extraGroups = ["wheel"];
-    packages = with pkgs; [
-    ];
+  users = {
+    defaultUserShell = pkgs.zsh;
+
+    users.alex = {
+      isNormalUser = true;
+      extraGroups = ["wheel"];
+      packages = with pkgs; [
+      ];
+    };
+
+    users.dimitar = {
+      isNormalUser = true;
+      extraGroups = ["wheel"];
+    };
+
   };
 
   environment.systemPackages = with pkgs; [
@@ -95,7 +105,39 @@
       "nvidia-persistenced"
     ];
 
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_390;
+  hardware = {
+    graphics.enable = true;
+    nvidia = {
+      # Modesetting is required.
+      modesetting.enable = true;
 
+      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+      # Enable this if you have graphical corruption issues or application crashes after waking
+      # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+      # of just the bare essentials.
+      powerManagement.enable = false;
+
+      # Fine-grained power management. Turns off GPU when not in use.
+      # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+      powerManagement.finegrained = false;
+
+      # Use the NVidia open source kernel module (not to be confused with the
+      # independent third-party "nouveau" open source driver).
+      # Support is limited to the Turing and later architectures. Full list of
+      # supported GPUs is at:
+      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+      # Only available from driver 515.43.04+
+      open = false;
+
+      # Enable the Nvidia settings menu,
+      # accessible via `nvidia-settings`.
+      nvidiaSettings = true;
+
+      # Optionally, you may need to select the appropriate driver version for your specific GPU.
+      package = config.boot.kernelPackages.nvidiaPackages.legacy_390;
+
+      # package = config.boot.kernelPackages.nvidiaPackages.legacy_390;
+    };
+  };
   system.stateVersion = "25.05";
 }
