c8ac2af06e5620ead51c69f45b3d8e1b324700c7
AleksandarZhekovski <aleksandarzhekovski@protonmail.com>
2025-09-26 00:26:19 +0300

    added za, ze and zr shorthand for ssh hosts

--- nixModules/ssh/ssh.nix
+++ nixModules/ssh/ssh.nix
@@ -12,8 +12,16 @@
       Hostname 10.244.0.200
       Port 2024
       User alex
+    Host ze
+      Hostname 10.244.0.200
+      Port 2024
+      User alex
 
     Host zRivendell
+      Hostname 10.244.0.100
+      Port 2024
+      User alex
+    Host zr
       Hostname 10.244.0.100
       Port 2024
       User alex
@@ -22,7 +30,11 @@
       Hostname 10.244.0.250
       Port 2024
       User alex
-    ";
+    Host za
+      Hostname 10.244.0.250
+      Port 2024
+      User alex
+     ";
 
   users.users.alex.openssh.authorizedKeys.keyFiles = [
     ./Erebor_pub_key
