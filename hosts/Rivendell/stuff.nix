02bfff4f3cf12026ceb6d4e8d7f0b6a439bc0339
AleksandarZhekovski <aleksandarzhekovski@protonmail.com>
2026-07-16 21:30:11 +0300

    temp: add citrix to Rivendell

--- hosts/Rivendell/stuff.nix
+++ hosts/Rivendell/stuff.nix
@@ -1,5 +1,17 @@
 { pkgs, pkgs-unstable, ... }:
 {
+  environment.systemPackages = with pkgs; [
+    citrix_workspace
+  ];
+
+  # In hosts/HOST/configuration.nix
+  # # Also enable non-free packages or else the factorio download will fail:
+  nixpkgs.config.allowUnfree = true;
+
+  nixpkgs.config.permittedInsecurePackages = [
+    "libsoup-2.74.3"
+  ];
+
   #TODO establish the firewall
   networking.firewall = {
     allowedUDPPorts = [
