11fddda2bb40c1f00d463f871c91299346b997e8
Alex <AleksandarZhekovski@prtonmail.com>
2026-08-06 21:57:15 +0300

    fix ssh defs

--- nixModules/ssh/default.nix
+++ nixModules/ssh/default.nix
@@ -13,16 +13,6 @@
   };
 
   programs.ssh.extraConfig = "
-    Host tErebor
-      Hostname 100.64.64.200
-      Port 2024
-    Host te
-      Hostname 10.244.0.200
-      Port 2024
-    Host le
-      Hostname 192.168.1.200
-      Port 2024
-
     Host tRivendell
       Hostname 100.64.64.100
       Port 2024
@@ -31,6 +21,16 @@
       Port 2024
     Host lr
       Hostname 192.168.1.100
+      Port 2024
+
+    Host tErebor
+      Hostname 100.64.64.200
+      Port 2024
+    Host te
+      Hostname 100.64.64.200
+      Port 2024
+    Host le
+      Hostname 192.168.1.200
       Port 2024
 
      ";
