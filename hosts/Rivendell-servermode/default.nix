06ff50c4541a75db735dd5d86cdf66f760ca9430
Alex <AleksandarZhekovski@prtonmail.com>
2026-08-17 20:27:54 +0300

    init Rivendell-servermode

---
+++ hosts/Rivendell-servermode/default.nix
@@ -0,0 +1,150 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+{
+  lib,
+  pkgs,
+  pkgs-unstable,
+  ...
+}:
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ./stuff.nix
+
+    ../../nixModules
+  ];
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      # initial_session = {
+      #   command = "${pkgs.hyprland}/bin/start-hyprland";
+      #   user = "alex";
+      # };
+      default_session = {
+        command = "${pkgs.zsh}/bin/zsh";
+        user = "alex";
+      };
+    };
+  };
+
+  hjem = {
+    clobberByDefault = true;
+    users = {
+      alex = {
+        enable = true;
+        user = "alex";
+        directory = "/home/alex";
+        # I don't like this
+        # files = {
+        #   ".config/hypr/hyprland.conf".source = ../../nixModules/hyprland/hypr/Rivendell.conf;
+        # };
+      };
+    };
+  };
+
+  basePkgs.enable = true;
+  # hyprland.enable = true;
+  tailscale.enable = true;
+  # verilog.enable = true;
+  # zerotier.enable = true;
+  # mc-server.enable = true;
+  git-server.enable = true;
+  # radicale.enable = true;
+
+  audio = {
+    enable = true;
+    roc-source = {
+      enable = true;
+      source-port = 4723;
+      repair-port = 4724;
+      name = "Rivendell ROC receiver source";
+      node-name = "Rivendell ROC teceiver";
+    };
+
+    roc-sink = {
+      enable = true;
+      remote-ip = "192.168.1.200";
+      source-port = 4713;
+      repair-port = 4714;
+      name = "Rivendell ROC transmiter source";
+      node-name = "Rivendell ROC transmiter";
+    };
+  };
+
+  gaming = {
+    enable = true;
+    steam = true;
+    # lutris = true;
+    # minecraft = true;
+  };
+
+  # Use the systemd-boot EFI boot loader.
+  boot = {
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    kernelPackages = pkgs.linuxPackages_latest;
+    initrd.kernelModules = [
+      "amdgpu"
+      "btusb"
+    ];
+    kernelParams = [ "mitigations=off" ];
+  };
+
+  networking = {
+    hostName = "Rivendell"; # Define your hostname.
+    networkmanager.enable = true; # Easiest to use and most distros use this by default.
+
+    interfaces = {
+      enp3s0 = {
+        wakeOnLan.enable = true;
+      };
+    };
+    firewall = {
+      allowedUDPPorts = [ 9 ];
+    };
+  };
+
+  time.timeZone = "Europe/Sofia";
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Experimental = true;
+        FastConnectable = true;
+      };
+      Policy = {
+        AutoEnable = true;
+      };
+    };
+  };
+
+  hardware.enableRedistributableFirmware = true;
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+    extraPackages = with pkgs; [
+      rocmPackages.clr.icd
+    ];
+  };
+
+  environment = {
+    sessionVariables.NIXOS_OZONE_WL = "1";
+    variables.TERM = "xterm-kitty";
+  };
+  environment.systemPackages =
+    (with pkgs; [
+      # kitty
+    ])
+    ++ (with pkgs-unstable; [
+      # pear-desktop
+      # vesktop
+    ]);
+
+  system.stateVersion = "24.11"; # tl;dr don't change this
+}
---
+++ hosts/Rivendell-servermode/stuff.nix
@@ -0,0 +1,29 @@
+{ pkgs, pkgs-unstable, ... }:
+{
+  # In hosts/HOST/configuration.nix
+  # # Also enable non-free packages or else the factorio download will fail:
+  nixpkgs.config.allowUnfree = true;
+
+  # nixpkgs.config.permittedInsecurePackages = [
+  #   "libsoup-2.74.3"
+  # ];
+  # environment.systemPackages = with pkgs; [
+  #   citrix_workspace
+  # ];
+
+
+  #TODO establish the firewall
+  networking.firewall = {
+    allowedUDPPorts = [
+      34197
+      2024
+      25565
+    ];
+    allowedTCPPorts = [
+      80
+      443
+      2024
+      25565
+    ];
+  };
+}
